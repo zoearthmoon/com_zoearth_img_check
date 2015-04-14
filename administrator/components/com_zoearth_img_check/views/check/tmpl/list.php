fda15754b2793eb820c00f7ec94d91dfae8be57d
Zhao Ren Zhang <zoearthmoon@gmail.com>
2015-04-14 14:44:33 +0800

    刪除圖片功能 可能有點麻煩 之後再補上 先以 搜尋與壓縮為主

--- administrator/components/com_zoearth_img_check/views/check/tmpl/list.php
+++ administrator/components/com_zoearth_img_check/views/check/tmpl/list.php
@@ -98,7 +98,7 @@ function selectAll(btnObj)
             <td>
                 <button type="button" class="btn btn-info actionBtns" disabled title="有時間在補上"><i class="icon-share-alt fa fa-share-square-o"></i>取代圖片</button>
                 <button type="button" class="btn btn-success actionBtns" onclick="actionImgs('replace_img_to_jpg')" ><i class="icon-list fa fa-database"></i>壓縮圖片</button>
-                <button type="button" class="btn btn-danger actionBtns" onclick="actionImgs('replace_img_to_null')" ><i class="icon-trash fa fa-trash"></i>刪除圖片</button>
+                <button type="button" class="btn btn-danger actionBtns" disabled title="有時間在補上" ><i class="icon-trash fa fa-trash"></i>刪除圖片</button>
             </td>
         </tr>
         </table>
